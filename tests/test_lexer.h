1281072bf816adfa048ac72307f361f4fa845f5c
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-05-10 21:25:36 -0400

    test: add testing framework (atleast the beginnings of it)

---
+++ tests/test_lexer.h
@@ -0,0 +1,20 @@
+#ifndef TEST_LEXER
+#define TEST_LEXER
+#include "../src/globals.h"
+#include <memory>
+#include <string>
+
+class TestCase {
+public:
+  TestCase(const std::string &, const std::string &, const std::string &);
+  TestCase(const std::string &, const std::string &,
+           std::unique_ptr<Token[]> &);
+  void checkResult() const;
+
+private:
+  std::string name;
+  std::string expected;
+  std::string result_recieved;
+};
+
+#endif // !TEST_LEXER
