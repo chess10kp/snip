ac01c0a07175888052845ab903c78e26be2ec371
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-10-01 00:28:04 -0400

    feat(semantic): create sym table class

--- src/semantic.h
+++ src/semantic.h
@@ -4,7 +4,9 @@
 #include "ast.h"
 #include "globals.h"
 #include "parser.h"
+#include <memory>
 #include <unordered_map>
+#include <variant>
 #include <vector>
 
 class SemanticAnalyzer {
@@ -15,14 +17,38 @@ private:
   ASTNode *root;
 };
 
+struct SymbolTableEntry {
+  ParserToken type;
+  std::variant<char, int, std::string> ident_value;
+};
+
 class SymbolTable {
 public:
   SymbolTable();
   ParserTokenChunk *get_tok() const;
-  void insert_tok(ParserTokenChunk);
+  int insert_tok(ParserTokenChunk *, PTNode *);
+  int insert_tok(PTNode *, PTNode *);
 
 private:
-  // std::unordered_map<std::string, ParserTokenChunk> table;
+  std::unordered_map<std::string, SymbolTableEntry> table;
+  // how to  i in sert an element into this the table?
+  int scope{0};
+};
+
+class SymbolTableS {
+public:
+  SymbolTableS();
+  ~SymbolTableS();
+  int insert(PTNode *, PTNode *);
+  void enter_scope();
+  void exit_scope();
+  int get_scope() const;
+  SymbolTable *get_top_table();
+  SymbolTable *get_current_scope();
+
+private:
+  std::vector<SymbolTable *> tables;
+  int scope = 0;
 };
 
 #endif
