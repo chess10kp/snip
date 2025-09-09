d17427da6973b23c1b096b4cd297e5f8f4932456
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2025-09-09 11:15:07 -0400

    feat: add statement types

--- Parser/Parser.cs
+++ Parser/Parser.cs
@@ -1,5 +1,31 @@
+using Snip.Lexer;
 namespace Snip.Parser;
 
 public class Parser
 {
+   private int _ptr = 0;
+   private Lexer.Lexer _lexer { get; set; }
+   private List<Token> _token { get; set; }
+   public Parser(Lexer.Lexer lexer)
+   {
+      _lexer = lexer;
+      _token = lexer.Tokenize();
+   }
+
+   public Token? peek()
+   {
+      if (_ptr < _token.Count)
+      {
+         Console.WriteLine("Peek called out of index");
+         return null;
+      }
+      return _token[_ptr++];
+   }
+
+   public Token? _next()
+   {
+      var token = peek();
+      _ptr++;
+      return token;
+   }
 }
